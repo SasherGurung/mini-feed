41c4cafb8f1d8642cb7b19ad7eeb8f1b827ff8d7
Sasher Gurung <sashergrg602@gmail.com>
2026-09-21 20:28:45 +0545

    fix: cleanup repo with proper .gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
-const app = require("./src/app");
-const { connectDb } = require("./src/db/pool");
+const express = require('express');
+require('dotenv').config();
 
-connectDb();
+const app = express();
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
