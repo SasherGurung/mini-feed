0af8b3c2c111579e5113b44de5923120d93f53c0
Sasher Gurung <sashergrg602@gmail.com>
2026-09-22 23:07:26 +0545

    feat(nav): completed UI component for Nav

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import Navbar from "@/components/layouts/Navbar";
 
 function MainPage() {
   return (
-    <main className='min-h-full'>
-      
+    <main className="h-screen grid grid-cols-[10%_50%_25%] gap-25">
+      <header className="border flex justify-center bg-zinc-50">
+        <Navbar />
+      </header>
+
+      <section className="border">
+        
+      </section>
+      <section className="border"></section>
     </main>
-  )
+  );
 }
 
 export default MainPage;
---
+++ frontend/components/layouts/Navbar.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import { FaHouse } from "react-icons/fa6";
+import { FaSearch } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { FaPlus } from "react-icons/fa6";
+import Link from "next/link";
+
+function Navbar() {
+  return (
+    <nav className="flex flex-col justify-between py-13 w-full px-1">
+      <div className="px-6">
+        <Image
+          src="/images/logo/logo.png"
+          alt="logo"
+          width={70}
+          height={70}
+          className="border cursor-pointer"
+        />
+      </div>
+
+      <ul className="flex flex-col">
+        <li>
+          <Link
+            href={""}
+            className="flex items-center gap-3 cursor-pointer hover:bg-zinc-100 px-6 py-3 rounded-xl"
+          >
+            <FaHouse className="h-6 w-6" />
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={""}
+            className="flex items-center gap-3 cursor-pointer hover:bg-zinc-100 px-6 py-3 rounded-xl"
+          >
+            <FaSearch className="h-6 w-6" />
+            Search
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={""}
+            className="flex items-center gap-3 cursor-pointer hover:bg-zinc-100 px-6 py-3 rounded-xl"
+          >
+            <FaHeart className="h-6 w-6 shrink-0" />
+            Notifications
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={""}
+            className="flex items-center gap-3 cursor-pointer hover:bg-zinc-100 px-6 py-3 rounded-xl"
+          >
+            <FaPlus className="h-6 w-6" />
+            Create
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={""}
+            className="flex items-center gap-3 cursor-pointer hover:bg-zinc-100 px-6 py-3 rounded-xl"
+          >
+            <CgProfile className="h-6 w-6" />
+            Profile
+          </Link>
+        </li>
+      </ul>
+
+      <div></div>
+    </nav>
+  );
+}
+
+export default Navbar;
